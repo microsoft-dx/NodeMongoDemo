1d46815d683b4d73ebd8105b7d57f93eba74c500
sorinpe <sorinpe@SORINPEYOGA>
2016-10-05 13:05:43 +0300

    insert data script

--- mongodb/insertdata.js
+++ mongodb/insertdata.js
@@ -1,12 +1,12 @@
 
 db.TodoItems.insertMany(
 [
-	  { name: "Send welcome emails", notes: "by tuesday", done: 0 },
-	  { name: "Talk to partners", notes: "", done: 0 },
-	  { name: "Go to the mountains", notes: "", done: 0 },
-	  { name: "Read a new book", notes: "", done: 0 },
-	  { name: "Run a marathon", notes: "", done: 1 },
-	  { name: "Pick up package", notes: "", done: 1 },
-	  { name: "Present @imworld", notes: "in October", done: 0 }
+	  { name: "Send welcome emails", notes: "by tuesday", done: false },
+	  { name: "Talk to partners", notes: "", done: false },
+	  { name: "Go to the mountains", notes: "", done: false },
+	  { name: "Read a new book", notes: "", done: false },
+	  { name: "Run a marathon", notes: "", done: true },
+	  { name: "Pick up package", notes: "", done: true },
+	  { name: "Present @imworld", notes: "in October", done: false }
 ]
 );
